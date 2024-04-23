2f5dd72ba250c426cfabf7a62c9c3e8268d5a1e9
Suryank1996 <suryankshashank@gmail.com>
2024-04-23 19:10:29 +0530

    change

--- src/main/java/com/restservices/Springboot/SpringbootApplication.java
+++ src/main/java/com/restservices/Springboot/SpringbootApplication.java
@@ -2,8 +2,7 @@ package com.restservices.Springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
-@SpringBootApplication
+//@SpringBootApplication
 public class SpringbootApplication {
 
 	public static void main(String[] args) 
