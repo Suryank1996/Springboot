1a1bc09838b502995f3dabdecc36dc848bbc423f
Suryank1996 <suryankshashank@gmail.com>
2024-04-23 18:40:06 +0530

    first commit

---
+++ src/test/java/com/restservices/Springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restservices.Springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
